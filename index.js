f12b8cd8bc988773fba232e09ed57ce509f4e3ee
tranphuong.080 <tranphuong.080@gmail.com>
2017-12-19 09:53:07 +0700

    change

--- index.js
+++ index.js
@@ -12,31 +12,31 @@ const qNetManager = require('./QNetManager')
 app.use(bodyParser.json({limit: '500mb'}))
 app.listen(10000, () => console.log('app listening on port 10000!'))
 
-// neatManager.initNeat({},()=>{
-  // neatManager.train()
-  // neatManager.startEvaluation({})
-// })
-qNetManager.initQNet({},()=>{
-  // setTimeout(()=>{
-      qNetManager.train()
-  // })
-
+neatManager.initNeat({},()=>{
+  neatManager.train()
+  neatManager.startEvaluation({})
 })
+// qNetManager.initQNet({},()=>{
+//   // setTimeout(()=>{
+//       qNetManager.train()
+//   // })
+//
+// })
 
 app.get('/api/bot_struct',(req,res)=>{
   let respone ={}
   switch (req.query.botType) {
     case 'NEAT':{
-      // let data = neatManager.getPlayer()
-      // if (data) {
-      //   respone.index = data.index
-      //   respone.tag = data.tag
-      //   console.log('sent ',data.index)
-      //   respone.network = data.data.toJSON()
-      //   res.send(JSON.stringify(respone))
-      // }else{
-      //   res.status(404).send('no available bot')
-      // }
+      let data = neatManager.getPlayer()
+      if (data) {
+        respone.index = data.index
+        respone.tag = data.tag
+        console.log('sent ',data.index)
+        respone.network = data.data.toJSON()
+        res.send(JSON.stringify(respone))
+      }else{
+        res.status(404).send('no available bot')
+      }
       break;
     }
     case 'QNET':{
@@ -50,7 +50,7 @@ app.get('/api/bot_struct',(req,res)=>{
 })
 
 app.post('/api/bot_struct',(req,res)=>{
-  // neatManager.setScore(req.body.index,req.body.tag,req.body.score)
+  neatManager.setScore(req.body.index,req.body.tag,req.body.score)
   res.send(JSON.stringify({OK:'OK'}))
 })
 
@@ -63,7 +63,7 @@ app.post('/api/training_data',(req,res)=>{
   console.log('received Data',req.body.data.length)
   switch (req.query.botType) {
     case 'NEAT':{
-      // neatManager.writeTrainingData(req.body.data)
+      neatManager.writeTrainingData(req.body.data)
       break;
     }
     case 'QNET':{
